a0c31d709ebb6bc9bca13859914f8c96e4be9450
Yahia Essam <yahia_essam1@siliconexpert.com>
2024-04-01 16:19:59 +0200

    generate user-module and shared module with shared modal component

---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gz-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
